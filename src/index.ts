27170540e80a958eb9b4feeddefd6f79109265fd
Tony Richards <trichards57@pm.me>
2026-01-06 16:15:31 +0000

    refactor(index): switch to use Date.now() directly for timing calc

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { set_user_input_dictionary } from "./matching/dictionary_match";
 import { most_guessable_match_sequence } from "./scoring";
 import { estimate_attack_times, type IAttackTimes } from "./time_estimates";
 
-const time = () => new Date().getTime();
 
 export interface IZXCVBNResult extends IAttackTimes {
   sequence: IAnyMatch[];
@@ -20,7 +19,7 @@ export function zxcvbn(
   password: string,
   user_inputs: (string | number | boolean)[] = [],
 ): IZXCVBNResult {
-  const start = time();
+  const start = Date.now();
   // reset the user inputs matcher on a per-request basis to keep things stateless
   const sanitized_inputs: string[] = [];
   for (const arg of user_inputs) {
@@ -30,7 +29,7 @@ export function zxcvbn(
 
   const matches = omnimatch(password);
   const result = most_guessable_match_sequence(password, matches);
-  const calc_time = time() - start;
+  const calc_time = Date.now() - start;
   const attack_times = estimate_attack_times(result.guesses);
   const fb = get_feedback(result.score, result.sequence);
 
