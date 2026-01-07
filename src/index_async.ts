6e6be503ae6b670d80dfc3e2c0eccbeebafdc2da
Tony Richards <trichards57@pm.me>
2026-01-07 17:34:55 +0000

    feat: adds async top-level function

---
+++ src/index_async.ts
@@ -0,0 +1,38 @@
+import { get_feedback, type IFeedbackItem } from "./feedback";
+import { type IAnyMatch, omnimatch } from "./matching";
+import { set_user_input_dictionary } from "./matching/dictionary_match_async";
+import { most_guessable_match_sequence } from "./scoring";
+import { estimate_attack_times, type IAttackTimes } from "./time_estimates";
+
+export interface IZXCVBNResult extends IAttackTimes {
+  sequence: IAnyMatch[];
+  guesses: number;
+  guesses_log10: number;
+  password: string;
+  score: number;
+  calc_time: number;
+  feedback: IFeedbackItem;
+}
+
+async function zxcvbn(
+  password: string,
+  user_inputs: (string | number | boolean)[] = []
+): Promise<IZXCVBNResult> {
+  const start = Date.now();
+  // reset the user inputs matcher on a per-request basis to keep things stateless
+  const sanitized_inputs: string[] = [];
+  for (const arg of user_inputs) {
+    sanitized_inputs.push(arg.toString().toLowerCase());
+  }
+  set_user_input_dictionary(sanitized_inputs);
+
+  const matches = omnimatch(password);
+  const result = most_guessable_match_sequence(password, matches);
+  const calc_time = Date.now() - start;
+  const attack_times = estimate_attack_times(result.guesses);
+  const fb = get_feedback(result.score, result.sequence);
+
+  return { ...result, ...attack_times, calc_time, feedback: fb };
+}
+
+export default zxcvbn;
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,30 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  platform: "neutral",
-  name: "zxcvbn",
-  globalName: "zxcvbn",
-  publint: true,
-  sourcemap: true,
-  exports: true,
-  dts: {
-    enabled: true,
+export default defineConfig([
+  {
+    platform: "neutral",
+    name: "zxcvbn",
+    globalName: "zxcvbn",
+    publint: true,
     sourcemap: true,
+    exports: true,
+    dts: {
+      enabled: true,
+      sourcemap: true,
+    },
   },
-});
+  {
+    entry: { index: "src/index_async.ts" },
+    platform: "neutral",
+    name: "zxcvbn",
+    globalName: "zxcvbn",
+    outDir: "dist/async",
+    publint: true,
+    sourcemap: true,
+    exports: true,
+    dts: {
+      enabled: true,
+      sourcemap: true,
+    },
+  },
+]);
